c1ca98da0200d2438a85254cd39b178b550da148
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-11-09 09:56:15 +1000

    yes

---
+++ main.js
@@ -0,0 +1,14 @@
+// Functions
+function bootupSystem() {
+    console.log("Attempting to bootup system:");
+    setTimeout(() => { console.log("    Booting up Nikhil Simulator."); }, 1000);
+    setTimeout(() => { console.log("    Booting up Nikhil Simulator.."); }, 1500);
+    setTimeout(() => { console.log("    Booting up Nikhil Simulator..."); }, 2000);
+    setTimeout(() => { console.log("Startup Complete!"); }, 3000);
+}
+
+// Start
+console.log("\nWelcome to Nikhil Simulator!");
+console.log("ⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺⲺ");
+
+setTimeout(() => { bootupSystem(); }, 2000);
