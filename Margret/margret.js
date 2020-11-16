58a3181e29ae365df395f8ff0a31bd6477d1a074
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-11-17 09:28:19 +1000

    Margret?

---
+++ Margret/margret.js
@@ -0,0 +1,81 @@
+// Variables
+let subjectsName = "Margret";
+let eventOutput;
+let eventRandomiser;
+let lifeSpan;
+let startLifeSpan;
+let eventsRun = 1;
+let increasedToTen = false;
+
+// Functions
+function bootupSystem() {
+    document.write("<br>Attempting to bootup system:");
+    setTimeout(() => { document.write("<br>    00% - Loading variables"); }, 1000);
+    setTimeout(() => { document.write("<br>    10% - Pre-running functions"); }, 1500);
+    setTimeout(() => { document.write("<br>    20% - Loading events"); }, 2000);
+    setTimeout(() => { document.write("<br>    30% - Finding a Moose"); }, 2500);
+    setTimeout(() => { document.write("<br>    40% - Returning values"); }, 3000);
+    setTimeout(() => { document.write("<br>    50% - Setting timeouts"); }, 3500);
+    setTimeout(() => { document.write("<br>    60% - Elsing ifs"); }, 4000);
+    setTimeout(() => { document.write("<br>    70% - Logging to the console"); }, 4500);
+    setTimeout(() => { document.write("<br>    80% - Bullying disrespectiful people"); }, 5000);
+    setTimeout(() => { document.write("<br>    90% - Finalising system bootup"); }, 5500);
+    setTimeout(() => { document.write("<br><br>>>> Startup Complete! <<<"); }, 6000);
+    setTimeout(() => { beginSimulation(); }, 9000);
+}
+
+function beginSimulation() {
+    lifeSpan = (Math.round(Math.random() * 50));
+
+    if (lifeSpan < 10) {
+        lifeSpan = lifeSpan + 24
+    }
+
+    startLifeSpan = lifeSpan;
+    document.write("<br><br>Beggining Simulation: (Total events: " + lifeSpan + ")");
+    setTimeout(() => { runSimulation(); }, 1000);
+}
+
+function runSimulation() {
+    simulateEvent();
+
+    if (eventsRun > 9) {
+        eventOutput = "{" + eventsRun + "/" + startLifeSpan + "}    $ " + subjectsName + " " + simulateEvent();
+    } else {
+        eventOutput = "{" + eventsRun + "/" + startLifeSpan + "}     $ " + subjectsName + " " + simulateEvent();
+    }
+
+    document.write("<br>" + eventOutput);
+
+    lifeSpan = lifeSpan - 1;
+    eventsRun = eventsRun + 1;
+
+    if (lifeSpan > 0) {
+        setTimeout(() => { runSimulation(); }, 1500);
+    } else {
+        setTimeout(() => { document.write("<br><br>>>> The Simulation Has Ended! <<<"); }, 2000);
+    }
+}
+
+function simulateEvent() {
+    eventRandomiser = (Math.round(Math.random() * 4));
+    if (eventRandomiser == 0) {
+        return "is a moose"
+    } else if (eventRandomiser == 1) {
+        return "got them free v-bucks"
+    } else if (eventRandomiser == 2) {
+        return "jumped off Mount Everest"
+    } else if (eventRandomiser == 3) {
+        return "spoke in a wierd voice"
+    } else if (eventRandomiser == 4) {
+        return "was disrespectful"
+    }
+}
+
+// Start
+function start() {
+    document.write("<br>Welcome to " + subjectsName + " Simulator!");
+    document.write("<br>------------------------------------------------------<br>");
+
+    setTimeout(() => { bootupSystem(); }, 2000);
+}
